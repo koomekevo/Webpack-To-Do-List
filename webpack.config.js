e271af3bfaac39ff4de09554ede4e81b4c456dfd
koomekevo <koome.kevo@gurucrafts.net>
2021-12-02 16:21:50 +0300

    Deploy to Github pages using gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: "/Webpack-To-Do-List/",
   },
   devServer: {
     static: './dist',
