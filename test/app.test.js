cf5e4cb0709099600e6ff3b924ba60a574248b81
Bohdan Shcherbak <bogdan.szczerbak@gmail.com>
2021-12-09 09:22:50 +0100

    Add the first test

--- test/app.test.js
+++ test/app.test.js
@@ -2,5 +2,10 @@ import { add, remove, } from '../src/crud';
 
 //Add tests
 describe('Tests for todo list app', () => {
-  
+  test('should add item to the list', () => {
+    add('First item');
+    add('Second item');
+    const storage = JSON.parse(localStorage.getItem('todoList'));
+    expect(storage).toHaveLength(2);
+  });
 });
