f239961452382269b96711f47dfe9aba4651c3ab
koomekevo <koome.kevo@gurucrafts.net>
2021-12-09 16:58:50 +0300

    Add update(), edit() and deleteAllCompleted() tests

--- test/app.test.js
+++ test/app.test.js
@@ -13,4 +13,23 @@ describe('Tests for todo list app', () => {
     const storage = JSON.parse(localStorage.getItem('todoList'));
     expect(storage).toHaveLength(1);
   });
+  test('Update complete status', () => {
+    const newItem = new ToDo('some text');
+    expect(newItem.complete).toBeFalsy();
+    newItem.update();
+    expect(newItem.complete).toBeTruthy();
+  });
+
+  test('Edit item', () => {
+    edit(0, 'edited text');
+    const storage = JSON.parse(localStorage.getItem('todoList'));
+    expect(storage[0].description).toBe('edited text');
+  });
+
+  test('Delete all completed', () => {
+    deleteAllCompleted(ToDo);
+    ToDo.list.forEach((item) => {
+      expect(item.complete).toBeFalsy();
+    });
+  });
 });
