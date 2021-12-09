7defe621f31d7c438f9d6f097c70a27b00ec65b9
koomekevo <koome.kevo@gurucrafts.net>
2021-12-09 10:26:50 +0300

    Add add() and remove() tests

---
+++ test/app.test.js
@@ -0,0 +1,16 @@
+import { add, remove, } from '../src/crud';
+  
+describe('Tests for todo list app', () => {
+  test('should add item to the list', () => {
+    add('First item');
+    add('Second item');
+    const storage = JSON.parse(localStorage.getItem('todoList'));
+    expect(storage).toHaveLength(2);
+  });
+  
+  test('Should remove an item from the list', () => {
+    remove(0);
+    const storage = JSON.parse(localStorage.getItem('todoList'));
+    expect(storage).toHaveLength(1);
+  });
+});
